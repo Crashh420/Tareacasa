9ab57342056e1073959770a3083cd234aca54681
Crashh420 <jmponton@unimagdalena.edu.co>
2024-11-24 17:06:24 -0500

    tareacasa

---
+++ js.js
@@ -0,0 +1,10 @@
+function validarFormulario(){
+    const name = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    if (name === '' || email === '') {
+   
+    alert('Por favor, completa todos los campos.');
+    } else {
+    alert('¡Formulario enviado correctamente!');
+    }
+   }
